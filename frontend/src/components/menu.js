46d426deeda9ade53255760b9927cae8f3a75625
Muffasa <superwubbq@gmail.com>
2018-07-04 00:39:55 +0800

    ok

--- frontend/src/components/menu.js
+++ frontend/src/components/menu.js
@@ -35,10 +35,11 @@ class Menu extends Component {
     return (
       <div>
         <ul id="menu">
-          <a href="#t1"><li className="icon" id="home"></li></a>
-          <a href="#t2"><li className="icon" id="search"></li></a>
-          <a href="#t3"><li className="icon" id="ranking"></li></a>
-          <a href="#t4"><li className="icon" id="user"></li></a>
+          <Link to="/"><li className="icon" id="home"></li></Link>
+          <Link to="/search" ><li className="icon" id="search"></li></Link>
+          <Link to="/login" ><li className="icon" id="ranking"></li></Link>
+          <Link to="/addform"><li className="icon" id="user"></li></Link>
+          
         </ul>
         <Snackbar
           anchorOrigin={{ vertical: 'top', horizontal: 'right' }}
@@ -76,4 +77,9 @@ export default connect(mapStateToProps, { userAuth })(Menu);
         <Link to="/search"><li className="icon" id="search"></li></Link>
         <Link to="/login"><li className="icon" id="ranking"></li></Link>
         <Link to="/addform"><li className="icon" id="user"></li></Link>
+
+          <a href="#t1"><li className="icon" id="home"></li></a>
+          <a href="#t2"><li className="icon" id="search"></li></a>
+          <a href="#t3"><li className="icon" id="ranking"></li></a>
+          <a href="#t4"><li className="icon" id="user"></li></a>
 */
